9134bdbffb171f8b3a631f241f3ff007fc1ed19e
Unknown <200mimi@mail.ru>
2018-01-22 03:47:41 +0300

    fix

--- Karatsuba__algorithm/main.cpp
+++ Karatsuba__algorithm/main.cpp
@@ -2,63 +2,43 @@
 #include <cmath>
 #include <iostream>
 
+#include "ArbitraryInt.h"
+
 using namespace std;
 
-size_t integer_length (long long number) {
-    size_t digits = 0;
-    
-    while (number) {
-        number /= 10;
-        digits++;
-    }
-    
-    return digits;
-}
-
-pair<long long, long long> split_integer_at (long long number, int position) {
-    long long high, low, base_power;
-    
-    base_power = (long long) pow(10, position);
-    
-    high = number / base_power;
-    low = number % base_power;
-    
-    return make_pair(high, low);
-}
-
-long long max (long long a, long long b) {
+int max (int a, int b) {
     return (a > b) ? a : b;
 }
 
 
-long long karatsuba_algorithm (long long a, long long b) {
+ArbitraryInt karatsuba_algorithm (const ArbitraryInt & a, const ArbitraryInt & b) {
     
-    if (a < 10 || b < 10) {
-        return a * b;
+    if (a.number_length() < 2 || b.number_length() < 2) {
+        return ArbitraryInt(a.to_integer() * b.to_integer());
     }
     
-    int size_max = (int) max(integer_length(a), integer_length(b));
+    int size_max = (int) max(a.number_length(), b.number_length());
     int half = size_max / 2;
     
-    long long high_a, low_a, high_b, low_b;
+    ArbitraryInt high_a, low_a, high_b, low_b;
     tie(high_a, low_a) = split_integer_at(a, half);
     tie(high_b, low_b) = split_integer_at(b, half);
     
-    long long z0, z1, z2;
+    ArbitraryInt z0, z1, z2;
     
     z0 = karatsuba_algorithm(low_a, low_b);
     z1 = karatsuba_algorithm(low_a + high_a, low_b + high_b);
     z2 = karatsuba_algorithm(high_a, high_b);
     
-    return (z2 * pow(10, 2 * half)) + ((z1 - z2 - z0) * pow(10, half)) + z0;
+    return shift(z2, 2 * half) + shift(z1 - z2 - z0, half) + z0;
 }
 
 
 int main () {
-    long long a, b;
+    string a, b;
     cout << "Enter two numbers:" << endl;
     while (cin >> a >> b) {
-        cout << karatsuba_algorithm(a, b) << endl;
+        cout << karatsuba_algorithm(ArbitraryInt(a),
+                                    ArbitraryInt(b)).repr() << endl;
     }
 }
-
